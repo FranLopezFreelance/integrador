836f5cb0a77abe57922dbb7734fa99a78b70ca3a
Fran <franlopez.freelance@gmail.com>
2016-07-25 18:36:55 -0300

    ultimo commit antes de integrar el theme nuevo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/home') }}">
+                <a class="navbar-brand" href="{{ (!Auth::guest()) ? url('/home') : url('/') }}">
                     Natural Market
                 </a>
             </div>
@@ -66,7 +66,7 @@
                         </ul>
                     </li>
 
-                    <li><a href="{{ url('/notifications/list') }}">Notificaciones</a></li>
+                    <li id="notifications"></li>
 
                     @endif
                 </ul>
@@ -107,6 +107,8 @@
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
 </body>
 </html>
