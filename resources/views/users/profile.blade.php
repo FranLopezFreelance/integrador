f80644bb5d9e61273ccb41b2e973198fca9dc6f4
Fran <franlopez.freelance@gmail.com>
2016-08-17 18:26:49 -0300

    el dia final...

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -14,7 +14,15 @@
               <div class="row">
                 <div class="col-md-4 col-md-offset-4">
 
-                  <img class="img-circle user-image" width="150" src="/{{ $user->avatar }}" />
+                  <img class="img-circle user-image" width="150"
+                              @if($user->avatar == 'images/users/default.png')
+
+                                  src="/{{ $user->avatar }}" />
+
+                              @else
+                                  src="{{ route('user.image', ['name' => $user->avatar]) }}" />
+
+                              @endif
 
                   </div>
               </div>
@@ -25,6 +33,27 @@
                   <p class="p-profile"><b>Perfil:</b> {{ $user->type->name }}</p>
                   <p class="p-profile"><b><a href="/products/{{ $user->id }}">Productos Publicados:</b></a> {{ $user->products()->count() }}</p>
                   <p class="p-profile"><b>Productos Vendidos:</b> {{ $user->sales()->count() }}</p>
+                </div>
+
+                <div class="profile-userbuttons">
+
+                  @if(!Auth::guest())
+                    <a class="btn unFollowButton btn-danger btn-sm
+                      @if(!$user->following->contains($user->id))
+                        button-hidden
+                      @endif
+                    " onclick="unfollow({{ $user->id }})">
+                      No seguir
+                    </a>
+                    <a class="btn followButton btn-success btn-sm
+                      @if($user->following->contains($user->id))
+                        button-hidden
+                      @endif"
+                      onclick="follow({{ $user->id }})">
+                        Seguir
+                    </a>
+                  @endif
+
                 </div>
 
                 <hr class="divider" />
@@ -58,6 +87,21 @@
                   <div class="row">
                     <div class="col-md-12">
                     @forelse($user->qualifySeller as $qualification)
+                      <p class="quali"><b>{{ $qualification->quality->name }}</b></p>
+                        <p class="comment-quali">{{ $qualification->comment }}</p>
+                        <p class="data-quali">
+                            <img class="img-circle" width="40"
+                                @if($qualification->user->avatar == 'images/users/default.png')
+
+                                    src="/{{ $qualification->user->avatar }}" />
+
+                                @else
+                                    src="{{ route('user.image', ['name' => $qualification->user->avatar]) }}" />
+
+                                @endif
+                          <a href="/users/profile/{{ $qualification->user->id }}">{{ $qualification->user->name }} {{ $qualification->user->lastname }}</a>
+                         {{ date('F d, Y', strtotime($qualification->created_at)) }}</p>
+                         <hr class="divider" />
                     @empty
                       <h5>Aún no fue calificado como Vendedor</h5>
                     @endif
@@ -68,6 +112,21 @@
                   <div class="row">
                     <div class="col-md-12">
                       @forelse($user->qualifyCustomer as $qualification)
+                        <p class="quali"><b>{{ $qualification->quality->name }}</b></p>
+                        <p class="comment-quali">{{ $qualification->comment }}</p>
+                        <p class="data-quali">
+                            <img class="img-circle" width="40"
+                                @if($qualification->user->avatar == 'images/users/default.png')
+
+                                    src="/{{ $qualification->user->avatar }}" />
+
+                                @else
+                                    src="{{ route('user.image', ['name' => $qualification->user->avatar]) }}" />
+
+                                @endif
+                          <a href="/users/profile/{{ $qualification->user->id }}">{{ $qualification->user->name }} {{ $qualification->user->lastname }}</a>
+                         {{ date('F d, Y', strtotime($qualification->created_at)) }}</p>
+                         <hr class="divider" />
                       @empty
                         <h5>Aún no fue calificado como Comprador</h5>
                       @endif
